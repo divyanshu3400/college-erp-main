69b427266372673b6e509efbd65340fc2309ef92
DKRBC3400 <divyanshu@risebeyond.in>
2023-10-12 20:03:31 +0530

    updated

--- student_management_app/admin.py
+++ student_management_app/admin.py
@@ -9,4 +9,11 @@ from student_management_app.models import CustomUser
 class UserModel(UserAdmin):
     pass
 
-admin.site.register(CustomUser,UserModel)
+# admin.site.register(CustomUser,UserModel)
+
+from django.apps import apps
+
+app_models = apps.get_app_config('student_management_app').get_models()
+
+for model in app_models:
+    admin.site.register(model)
